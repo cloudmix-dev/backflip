000f1ecca78acde35139ce0889a390cb3cc0b7cf
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-12-21 15:30:05 +0000

    docs: rename expo example backflip files

--- examples/expo/app/api/components/[component]+api.ts
+++ examples/expo/app/api/components/[component]+api.ts
@@ -1,6 +1,6 @@
 import { type ExpoRequest } from "expo-router/server";
 
-import { server } from "../../../backflip/backflip.server";
+import { server } from "../../../backflip/server";
 
 export function GET(request: ExpoRequest) {
   return server.fetch(request as unknown as Request);
