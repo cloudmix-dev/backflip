5bac1b0be28019a584f36e358c6dced203c719a6
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-12-31 16:29:23 +0000

    build: update docs build setup

--- www/docs/src/server.ts
+++ www/docs/src/server.ts
@@ -1,11 +1,16 @@
 // wrangler will bundle this "server.mjs" with prebuilt `remixBuild`
 import { createRequestHandler } from "@remix-run/server-runtime";
+
 import * as remixBuild from "../build/server/index.js";
 
-const remixHandler = createRequestHandler(remixBuild, process.env.NODE_ENV);
+const remixHandler = createRequestHandler(
+  // biome-ignore lint/suspicious/noExplicitAny: we don't have the type for this
+  remixBuild as any,
+  process.env.NODE_ENV,
+);
 
 export default {
-  fetch(request, env, _ctx) {
+  fetch(request: Request, env: Record<string, unknown>) {
     Object.assign(globalThis, { env });
 
     return remixHandler(request);
