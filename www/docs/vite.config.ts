f64f618e5e20a17a09e7b7d965058e53742dfc1f
Sam Laycock <samuel.paul.laycock@gmail.com>
2024-01-01 13:21:40 +0000

    docs: move docs content to markdown

--- www/docs/vite.config.ts
+++ www/docs/vite.config.ts
@@ -1,4 +1,8 @@
+import mdx from "@mdx-js/rollup";
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import rehypeSlug from "rehype-slug";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -6,6 +10,10 @@ export default defineConfig({
   plugins: [
     remix({
       appDirectory: "./src",
+    }),
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [rehypeSlug],
     }),
   ],
 });
